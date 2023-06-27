ca6847250a90193fcd17d641e00da8c2de055ce9
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-06-26 22:03:44 -0700

    Add function to request a single layout
    Also add extremely simple debugging flag

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -9,6 +12,8 @@ import (
 )
 
 const baseURL = "https://my.glove80.com/api/layouts/v1/"
+
+var debug = false
 
 type Layout struct {
 	Metadata struct {
@@ -30,18 +35,62 @@ type Layout struct {
 	CompilerInput any `json:"compiler_input"`
 }
 
-func main() {
-	if len(os.Args) > 2 {
-		log.Fatalf("%s only takes 1 argument at most (a comma separated list of tags to search for)", os.Args[0])
-	}
-	searchUrl, err := url.Parse(baseURL)
+func getLayout(uid string) Layout {
+	layoutURL, err := url.Parse(baseURL)
 	if err != nil {
 		panic(err)
 	}
-	if len(os.Args) == 2 {
-		tags := os.Args[1]
-		searchUrl.Query().Add("tags", tags)
+	layoutURL.Path, err = url.JoinPath(layoutURL.Path, uid, "meta")
+	if err != nil {
+		panic(err)
 	}
-	resp, err := http.Get(searchUrl.String())
-	io.Copy(os.Stdout, resp.Body)
+	log.Printf("Requesting layout: %s", layoutURL.String())
+	resp, err := http.Get(layoutURL.String())
+	if err != nil {
+		panic(err)
+	}
+	layoutBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	result := Layout{}
+	json.Unmarshal(layoutBytes, &result)
+	return result
+}
+
+func main() {
+	flag.BoolVar(&debug, "debug", false, "Whether to print debug statements")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 1 {
+		log.Fatalf("%s only takes 1 argument at most (a comma separated list of tags to search for)", os.Args[0])
+	}
+	if !debug {
+		// this is a script, so we're just going to panic if anything goes
+		// wrong. I.e. all logs are for debugging.
+		log.Default().SetOutput(io.Discard)
+	}
+
+	searchURL, err := url.Parse(baseURL)
+	if err != nil {
+		panic(err)
+	}
+	if len(args) == 1 {
+		tags := args[0]
+		query := url.Values{}
+		query.Add("tags", tags)
+		searchURL.RawQuery = query.Encode()
+	}
+	log.Printf("Requesting layout unique IDs: %s", searchURL.String())
+	resp, err := http.Get(searchURL.String())
+	var uids []string
+	uidBytes, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(uidBytes, &uids)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", getLayout(uids[0]))
 }
